2cdce12cb65a4f546ad06b2c6f99e2e2e4eb30bf
OsmanRana <osmancmc@yahoo.com>
2022-02-04 18:11:13 +0600

    login added

--- src/pages/Services/SeviceDetail/Service.js
+++ src/pages/Services/SeviceDetail/Service.js
@@ -1,12 +1,14 @@
-import { Container } from "@mui/material";
-import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { Button, Container, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 
 const Service = () => {
   const [service, setService] = useState([]);
+  const { name, description, image, slogan } = service;
   const { serviceId } = useParams();
-  console.log(service)
+  console.log(service);
   useEffect(() => {
     fetch(`http://localhost:5000/services/${serviceId}`)
       .then((res) => res.json())
@@ -14,8 +16,62 @@ const Service = () => {
   }, [serviceId]);
   return (
     <Container sx={{ height: "100%" }}>
-      <Box sx={{ my: 16 }}>
-        <h1>Single Service{serviceId}</h1>
+      <Box
+        sx={{
+          my: 16,
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center",
+          "& > :not(style)": {
+            m: 1,
+            width: 800,
+            height: 800,
+          },
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src={image} alt="Delivery" width="100%" />
+          <Typography
+            variant="h1"
+            component="div"
+            gutterBottom
+            fontWeight="bold"
+            sx={{ fontSize: { xs: 18 } }}
+          >
+            {slogan}
+          </Typography>
+
+          <Button
+            //   onClick={handleCloseNavMenu}
+            fullWidth
+            sx={{
+              m: 2,
+              p: 2,
+              color: "white",
+              display: "block",
+              backgroundColor: "#d21d24",
+              maxWidth: 180,
+              border: 1,
+              "&:hover": {
+                backgroundColor: "white",
+                color: "#d21d24",
+                borderColor: "#d21d24",
+                boxShadow: "none",
+              },
+            }}
+          >
+            Book Now
+          </Button>
+        </Paper>
       </Box>
     </Container>
   );
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import { Button, Typography } from "@mui/material";
+import { Alert, Button, Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
+import useFirebase from "../../hooks/useServices/useFibrebase";
 
 const SignUp = () => {
+  const [signUpData, setSignUpData] = useState({});
+  const { signUpNewUserWithEmailandPassword, authError, user } = useFirebase();
+  const handleOnBlur = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+    const newSignUpData = { ...signUpData };
+    newSignUpData[field] = value;
+    setSignUpData(newSignUpData);
+  };
+
+  const handleOnSubmit = (e) => {
+    signUpNewUserWithEmailandPassword(
+      signUpData.email,
+      signUpData.password,
+      signUpData.name
+    );
+    e.preventDefault();
+  };
   return (
     <Box
       sx={{
@@ -38,48 +57,65 @@ const SignUp = () => {
         >
           Sign Up
         </Typography>
-        <TextField
-          id="outlined-basic"
-          label="Name"
-          variant="outlined"
-          fullWidth
-          sx={{ maxWidth: 500, my: 4 }}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-          fullWidth
-          sx={{ maxWidth: 500 }}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Password"
-          variant="outlined"
-          fullWidth
-          sx={{ maxWidth: 500, my: 4 }}
-        />
-        <Button
-          //   onClick={handleCloseNavMenu}
-          fullWidth
-          sx={{
-            m: 2,
-            p: 2,
-            color: "white",
-            display: "block",
-            backgroundColor: "#d21d24",
-            maxWidth: 180,
-            border: 1,
-            "&:hover": {
-              backgroundColor: "white",
-              color: "#d21d24",
-              borderColor: "#d21d24",
-              boxShadow: "none",
-            },
-          }}
-        >
-          Sign up
-        </Button>
+        {user?.email && <Alert>Signed Up successsfully</Alert>}
+        {authError && <Alert severity="error">{authError}</Alert>}
+        <form onSubmit={handleOnSubmit}>
+          <TextField
+            id="name"
+            label="Name"
+            name="name"
+            type="name"
+            variant="outlined"
+            fullWidth
+            required
+            onBlur={handleOnBlur}
+            sx={{ maxWidth: 500, my: 4 }}
+          />
+          <TextField
+            id="email"
+            label="Email"
+            name="email"
+            type="email"
+            variant="outlined"
+            fullWidth
+            required
+            onBlur={handleOnBlur}
+            sx={{ maxWidth: 500 }}
+          />
+          <TextField
+            id="password"
+            label="Password"
+            name="password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            required
+            onBlur={handleOnBlur}
+            sx={{ maxWidth: 500, my: 4 }}
+          />
+
+          <Button
+            fullWidth
+            type="submit"
+            sx={{
+              m: 2,
+              p: 2,
+              color: "white",
+              display: "block",
+              backgroundColor: "#d21d24",
+              maxWidth: 180,
+              border: 1,
+              "&:hover": {
+                backgroundColor: "white",
+                color: "#d21d24",
+                borderColor: "#d21d24",
+                boxShadow: "none",
+              },
+            }}
+          >
+            Sign up
+          </Button>
+        </form>
         <Typography variant="caption" display="block" gutterBottom>
           Already have an account? Sign In now
         </Typography>
